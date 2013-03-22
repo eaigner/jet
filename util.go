35aed1b67c60e7381ebbfd70323cb3d2a8ef2a37
Erik Aigner <erik@chocomoko.com>
2013-03-22 17:36:15 +0100

    use timestamp as seed

--- util.go
+++ util.go
@@ -2,14 +2,18 @@ package jet
 
 import (
 	"math/rand"
+	"time"
 )
 
-var alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ1234567890"
+var (
+	gen   = rand.New(rand.NewSource(time.Now().UnixNano()))
+	alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ1234567890"
+)
 
 func newAlphanumericId(length int) string {
 	buf := make([]byte, length)
 	for i := 0; i < length; i++ {
-		buf[i] = alpha[rand.Intn(len(alpha))]
+		buf[i] = alpha[gen.Intn(len(alpha))]
 	}
 	return string(buf)
 }
