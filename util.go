ee3b51d9812fadc9ed639d21ba6c31aaa4e27747
Erik Aigner <erik@chocomoko.com>
2013-03-22 17:37:26 +0100

    const

--- util.go
+++ util.go
@@ -6,11 +6,11 @@ import (
 )
 
 var (
-	gen   = rand.New(rand.NewSource(time.Now().UnixNano()))
-	alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ1234567890"
+	gen = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
 func newAlphanumericId(length int) string {
+	const alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ1234567890"
 	buf := make([]byte, length)
 	for i := 0; i < length; i++ {
 		buf[i] = alpha[gen.Intn(len(alpha))]
