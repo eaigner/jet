70fd6562cdf3f0cb1ea11c0c25a4fa46644d0815
Erik Aigner <erik@chocomoko.com>
2013-08-21 23:37:58 +0200

    linebreak for readability

--- types.go
+++ types.go
@@ -7,7 +7,8 @@ import (
 type Runnable interface {
 	// Run runs the query without returning results
 	Run() error
-	// Rows runs the query writing the rows to the specified map or struct array. If maxRows is specified, only writes up to maxRows rows.
+	// Rows runs the query writing the rows to the specified map or struct array.
+	// If maxRows is specified, only writes up to maxRows rows.
 	Rows(v interface{}, maxRows ...int64) error
 }
 
